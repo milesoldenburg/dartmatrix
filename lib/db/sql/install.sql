39033ba475f78d6cb344a2a9a1ecbc8fac64c7cb
Miles Oldenburg <milesoldenburg@gmail.com>
2016-03-07 22:46:23 -0500

    Added draft of settings page

--- lib/db/sql/install.sql
+++ lib/db/sql/install.sql
@@ -6,3 +6,10 @@ CREATE TABLE IF NOT EXISTS "gametypes" (
 INSERT INTO "gametypes" VALUES (1, 'Cricket');
 INSERT INTO "gametypes" VALUES (2, '301');
 INSERT INTO "gametypes" VALUES (3, '501');
+
+CREATE TABLE IF NOT EXISTS "settings" (
+  "ai_enabled" INTEGER NOT NULL,
+  "ai_level" INTEGER NOT NULL
+);
+
+INSERT INTO "settings" VALUES (1, 40);
